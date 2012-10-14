082e8c4eafc1a42311ef53c930ee67057f3e2482
Tim Whitaker <tmwhtkr@gmail.com>
2012-10-14 13:20:43 -0700

    Anchor app started

---
+++ Anchor/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anchor::Application.config.secret_token = 'd7bcfb4fee0b73179056a37bf30b1639ef9b5870ea7c7e8df1386f85fb61430840c82966bbeb889ce001b2502cbe7550cdffbecea8caca0e66c4ed6af73bb913'
