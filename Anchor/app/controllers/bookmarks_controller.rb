9fc7db613c03801c9b97803130fae6ec3f6ac51e
Tim Whitaker <tmwhtkr@gmail.com>
2012-10-16 10:31:40 -0700

    last commit didnt add all files. Here's the rest

--- Anchor/app/controllers/bookmarks_controller.rb
+++ Anchor/app/controllers/bookmarks_controller.rb
@@ -6,22 +6,25 @@ class BookmarksController < ApplicationController
         redirect_to bookmarks_url, :alert => "Created Successfully"
     end
     def new
+        render :template => "bookmarks/new.html.erb"
     end
     def edit
+        render :template => "bookmarks/edit.html.erb"
     end
     def show
         @bookmark = all_bookmarks[params[:id].to_i]
     end
     def update
-        flash[:alert] = "Updated Successfully"
+        redirect_to bookmark_url(params[:id]), :alert => "Updated Successfully"
     end
     def destroy
+        redirect_to bookmarks_url, :alert => "Deleted Bookmark"
     end
 
     private
 
     def all_bookmarks
-        [{:url => "", :title => "testing 1", :notes => "", :tags => "", :rating => "", :public =>""},
-         {:url => "", :title => "testing 2", :notes => "", :tags => "", :rating => "", :public =>""}]
+        [{:url => "http://www.google.com", :title => "Google", :notes => "awesome search", :screenthumb => "linkthumb.jpg", :screenshot => "anchorscreenshot.png", :tags => {"Anchor", "Chico"}, :rating => "4.5", :public =>"#"},
+         {:url => "http://www.amazon.com", :title => "Amazon", :notes => "awesome goods", :screenthumb => "linkthumb.jpg", :screenshot => "anchorscreenshot.png", :tags => {"Awesome", "Art"}, :rating => "3", :public =>"\""}]
     end
 end
--- Anchor/app/controllers/users_controller.rb
+++ Anchor/app/controllers/users_controller.rb
@@ -1,18 +1,33 @@
 class UsersController < ApplicationController
     def index
+        @users = all_users
     end
     def create
+        redirect_to users_url, :alert => "Created Successfully"
     end
     def new
+        render :template => "users/new.html.erb"
     end
     def edit
+        render :template => "users/edit.html.erb"
     end
     def show
+        @user = all_users[params[:id].to_i]
     end
     def update
+        redirect_to user_url(params[:id]), :alert => "Updated Successfully"
     end
     def destroy
+        redirect_to users_url, :alert => "Deleted User"
     end
     def signup
     end
+
+    private
+
+    def all_users
+        [{:username => "Tim", :password => "abc123", :email => "tim@anchor.io", :first_name => "Tim", :last_name => "Whitaker", :avatar => "joeavatar.jpg"},
+         {:username => "Joe", :password => "abc123", :email => "Joe@anchor.io", :first_name => "Joe", :last_name => "User", :avatar => "joeavatar.jpg"}]
+    end
 end
+
